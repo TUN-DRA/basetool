ae88e24c57ef8bd4bf66bbc9e74b6658a8bb42bb
TUN-DRA <tkm.1sfan1028@gmail.com>
2021-08-28 11:38:44 +0900

    投稿表示機能の改善

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @tools = @user.tools
+    @tools = @user.tools.order('created_at DESC')
   end
 end
